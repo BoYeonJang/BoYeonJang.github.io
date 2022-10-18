f31066dab58901b1a9907f9516b5dda2fa264792
BoYeonJang <uko010199@gmail.com>
2022-10-18 22:20:42 +0900

    chore: uninstall @astrojs/image and sharp astro의 이미지 npm 삭제

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config'
-import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
@@ -7,10 +6,5 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://BoYeonJang.github.io',
-  integrations: [
-    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
-    mdx(),
-    sitemap(),
-    tailwind()
-  ]
+  integrations: [mdx(), sitemap(), tailwind()]
 })
